b3f8795343a65b2534dcb20defeb1f95254a8b4c
Kevin Cho <chokevinj@gmail.com>
2016-04-27 19:11:21 -0400

    Change logo hover portrait images

--- js/home.js
+++ js/home.js
@@ -3,15 +3,15 @@
 $(document).ready(function() {
   $('#icon-github').hover(function() {
     $('#link-description').html('Beware of awful high school code.');
-    $('.flipper-front').css('background-image', 'url(images/octocat/original-circle.png)');
+    $('.flipper-front').css('background-image', 'url(images/github-logo.png)');
   });
   $('#icon-linkedin').hover(function() {
     $('#link-description').html('Work + education. My professional life.');
-    $('.flipper-front').css('background-image', 'url(images/octocat/octoclark-kentocat-circle.png)');
+    $('.flipper-front').css('background-image', 'url(images/formal-logo.png)');
   });
   $('#icon-steam').hover(function() {
     $('#link-description').html('I do have a life outside of school and work ;)');
-    $('.flipper-front').css('background-image', 'url(images/octocat/jetpacktocat-circle.png)');
+    $('.flipper-front').css('background-image', 'url(images/gaming-logo.png)');
   });
   $('#icon-hummingbird').hover(function() {
     $('#link-description').html('Many hours invested in watching anime.');
