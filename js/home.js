9091a5e9f40a2e27bc0a4e19d317e9616245fe3f
Kevin Cho <chokevinj@gmail.com>
2016-04-28 15:23:50 -0400

    Add fade to logo hover

--- js/home.js
+++ js/home.js
@@ -1,28 +1,37 @@
-"use strict";
+(function() {
+  "use strict";
 
-$(document).ready(function() {
-  $('#icon-github').hover(function() {
-    $('#link-description').html('Beware of awful high school code.');
-    $('.flipper-front').css('background-image', 'url(images/github-logo.png)');
-  });
-  $('#icon-linkedin').hover(function() {
-    $('#link-description').html('Work + education. My professional life.');
-    $('.flipper-front').css('background-image', 'url(images/formal-logo.png)');
-  });
-  $('#icon-steam').hover(function() {
-    $('#link-description').html('I do have a life outside of school and work ;)');
-    $('.flipper-front').css('background-image', 'url(images/gaming-logo.png)');
-  });
-  $('#icon-hummingbird').hover(function() {
-    $('#link-description').html('Many hours invested in watching anime.');
-    $('.flipper-front').css('background-image', 'url(images/hummingbird-logo.png)');
-  });
-  $('#icon-email').hover(function() {
-    $('#link-description').html('Contact me if you wish, cheers.');
-    $('.flipper-front').css('background-image', 'url(images/mail-logo.png)');
-  });
+  var fadeOutVal = 150;
+  var fadeInVal = 250;
 
-  $('.flipper').mouseout(function() {
-    $('.flipper-front').css('background-image', 'url(images/kevin-2.png)');
+  function changeOnMouseover(text, image) {
+    $('#link-description').html(text);
+    
+    $('.flipper-front').fadeOut(fadeOutVal, function() {
+      $('.flipper-front').css('background-image', 'url(images/' + image + ')');
+    });
+    $('.flipper-front').fadeIn(fadeInVal);
+  }
+
+  $(document).ready(function() {
+    $('#icon-github').mouseover(function() {
+      changeOnMouseover('Beware of awful high school code.', 'github-logo.png');
+    });
+    $('#icon-linkedin').mouseover(function() {
+      changeOnMouseover('Work + education. My professional life.', 'formal-logo.png');
+    });
+    $('#icon-steam').mouseover(function() {
+      changeOnMouseover('I do have a life outside of school and work ;)', 'gaming-logo.png');
+    });
+    $('#icon-hummingbird').mouseover(function() {
+      changeOnMouseover('Many hours invested in watching anime.', 'hummingbird-logo.png');
+    });
+    $('#icon-email').mouseover(function() {
+      changeOnMouseover('Contact me if you wish, cheers.', 'mail-logo.png');
+    });
+
+    $('.flipper').mouseout(function() {
+      $('.flipper-front').css('background-image', 'url(images/kevin-2.png)');
+    });
   });
-});
+})();
