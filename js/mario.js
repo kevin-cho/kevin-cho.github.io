3e3be19c08af4bc2ae9f58cff77fc23eb37a9c57
Kevin Cho <chokevinj@gmail.com>
2016-10-25 14:57:34 -0400

    Update mario.js

--- js/mario.js
+++ js/mario.js
@@ -1,6 +1,7 @@
-(function() {
-    "use strict";
+(function($) {
+  "use strict";
 
+  $.fn.marioItemBlock = function() {
     var imgMarioBasicStand = "http://i.imgur.com/zm6xmOT.jpg";
     var imgMarioBasicJump = "http://i.imgur.com/BGYhIM1.png";
     var imgItemBlock = "http://i.imgur.com/Gc24df8.png";
@@ -13,93 +14,92 @@
     var previousValue = 0;
     var isKeyDown = false;
 
+    var gameTemplate = '<div id="gameArea" class="centreContent showContent"><img id="gameItem" src="http://i.imgur.com/ckC1CKK.png" class="centreContent showContent land"></img><img id="gameItemBlock" src="http://i.imgur.com/Gc24df8.png" class="centreContent showContent "></img><img id="gameSprite" src="http://i.imgur.com/zm6xmOT.jpg" class="centreContent showContent stand" align="bottom"></img></div>';
+
     $(document).keydown(function(e) {
-        // Space key
-        if (e.which == 32 && !isKeyDown) {
-            jump();
-        }
-        return false;
+      // Space key
+      if (e.which == 32 && !isKeyDown) {
+        jump();
+      }
+      return false;
     });
 
     $(document).keyup(function(e) {
-        // Space key
-        if (e.which == 32 && isKeyDown) {
-            stand();
-        }
-        return false;
+      // Space key
+      if (e.which == 32 && isKeyDown) {
+        stand();
+      }
+      return false;
     });
 
     $(document).mousedown(function() {
-        if (!isKeyDown) {
-            jump();
-        }
-        return false;
+      if (!isKeyDown) {
+        jump();
+      }
+      return false;
     });
 
     $(document).mouseup(function() {
-        if (isKeyDown) {
-            stand();
-        }
-        return false;
+      if (isKeyDown) {
+        stand();
+      }
+      return false;
     });
 
     function jump() {
-        var sprite = $("#gameSprite");
-        sprite.attr("src", imgMarioBasicJump);
-        sprite.removeClass("stand");
-        sprite.addClass("jump");
+      var sprite = $("#gameSprite");
+      sprite.attr("src", imgMarioBasicJump);
+      sprite.removeClass("stand");
+      sprite.addClass("jump");
 
-        $("#gameItem").attr("src", getItem());
-        toggleItem();
+      $("#gameItem").attr("src", getItem());
+      toggleItem();
 
-        isKeyDown = true;
+      isKeyDown = true;
     }
 
     function stand() {
-        var sprite = $("#gameSprite");
-        sprite.attr("src", imgMarioBasicStand);
-        sprite.removeClass("jump");
-        sprite.addClass("stand");
+      var sprite = $("#gameSprite");
+      sprite.attr("src", imgMarioBasicStand);
+      sprite.removeClass("jump");
+      sprite.addClass("stand");
 
-        toggleItem();
+      toggleItem();
 
-        isKeyDown = false;
+      isKeyDown = false;
     }
 
     function toggleItem() {
-        var item = $("#gameItem");
-        item.toggleClass("float");
-        item.toggleClass("land");
+      var item = $("#gameItem");
+      item.toggleClass("float");
+      item.toggleClass("land");
     }
 
     function getItem() {
-        var currentValue;
-        do {
-            currentValue = Math.floor((Math.random() * 6) + 1);
-        } while (currentValue === previousValue);
-        previousValue = currentValue;
+      var currentValue;
+      do {
+        currentValue = Math.floor((Math.random() * 6) + 1);
+      } while (currentValue === previousValue);
+      previousValue = currentValue;
 
-        switch (currentValue) {
-            case 1:
-                return imgFireFlower;
-            case 2:
-                return imgCoin;
-            case 3:
-                return imgStar;
-            case 4:
-                return imgMushroomRed;
-            case 5:
-                return imgMushroomGreen;
-            case 6:
-                return imgGoomba;
-            default:
-                return imgCoin;
-        }
+      switch (currentValue) {
+        case 1:
+          return imgFireFlower;
+        case 2:
+          return imgCoin;
+        case 3:
+          return imgStar;
+        case 4:
+          return imgMushroomRed;
+        case 5:
+          return imgMushroomGreen;
+        case 6:
+          return imgGoomba;
+        default:
+          return imgCoin;
+      }
     }
-})();
 
-var gameTemplate = '<div id="gameArea" class="centreContent showContent"><img id="gameItem" src="http://i.imgur.com/ckC1CKK.png" class="centreContent showContent land"></img><img id="gameItemBlock" src="http://i.imgur.com/Gc24df8.png" class="centreContent showContent "></img><img id="gameSprite" src="http://i.imgur.com/zm6xmOT.jpg" class="centreContent showContent stand" align="bottom"></img></div>';
-
-function marioInit(id) {
-    $(id).append(gameTemplate);
-}
+    this.append(gameTemplate);
+  }
+})(jQuery);
